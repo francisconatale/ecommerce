4a2653fda4600f09066276d73755e1cb93acb709
Francisco Natale <francisco_natale@hotmail.com>
2026-08-31 21:00:24 -0300

    Refactor: remove unused custom pagination abstractions
    As documented in ADR-0001, we pragmatically accepted the use of Spring's Pageable in the domain, making DomainPage and PaginationQuery obsolete.

--- backend/src/main/java/com/eshop/domain/base/PaginationQuery.java
+++
@@ -1,9 +0,0 @@
-package com.eshop.domain.base;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-@Data
-@AllArgsConstructor
-public class PaginationQuery {
-    private int page;
-    private int size;
-}
